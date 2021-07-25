efeeebfef6d57085691132e375802b3eec42a068
mstrivens <max.strivens@live.co.uk>
2021-07-25 17:56:06 +0100

    Refactored move method to be more readable.

--- lib/snakes_and_ladders.rb
+++ lib/snakes_and_ladders.rb
@@ -11,11 +11,20 @@ class SnakesAndLadders
   def move
     current_move = []
     current_move.push(dice, dice)
-    roll_total = current_move.inject(:+)
-    @player_1_position += roll_total
+    set_roll_total_from(current_move)
+    add_to_player_1_position
   end
 
   def position
     @player_1_position
   end
+
+private
+    def set_roll_total_from(current_move)
+      @roll_total = current_move.inject(:+)
+    end
+
+    def add_to_player_1_position
+      @player_1_position += @roll_total
+    end
 end
